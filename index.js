fc5912559aa41f90095e602a68201cbc8c1342a6
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-20 21:24:31 +0600

    Created Environment file and Mongodb credentials hided

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 const shopByCategory = require("./ShopByCategory.json")
 
 
-const uri = "mongodb+srv://75mdnazmulhasan:<password>@cluster0.4botfbx.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4botfbx.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
