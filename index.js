b6085f088bc6af1ffc2406638c655acf7814b77b
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-20 20:34:56 +0600

    dotenv is added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 7000;
 
@@ -9,6 +11,33 @@ app.use(express.json());
 
 
 const shopByCategory = require("./ShopByCategory.json")
+
+
+const uri = "mongodb+srv://75mdnazmulhasan:<password>@cluster0.4botfbx.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
     res.send('user Manegement Server is running')
